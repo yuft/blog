75a7ab1e5a515030c21ae9a78289b01af8614f90
yuft <daniel.yuft@gmail.com>
2022-04-24 15:30:58 +0930

    init commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+ module.exports = {
+  images: {
+    loader: "imgix",
+    path: "",
+  }
+}
---
+++ pages/about.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export const About = (): JSX.Element => {
+  return (
+    <Layout
+      customMeta={{
+        title: 'About - Hunter Chang',
+      }}
+    >
+      <h1>Daniel Yu</h1>
+      <p></p>
+    </Layout>
+  );
+};
+
+export default About;
